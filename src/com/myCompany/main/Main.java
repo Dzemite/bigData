6303f5f3b872f151ae401b2d998beb69f34b7298
Dzemite <nikol344265@rambler.ru>
2016-11-06 20:07:03 +0300

    start work

---
+++ src/com/myCompany/main/Main.java
@@ -0,0 +1,10 @@
+package com.myCompany.main;
+
+/**
+ * Created by Romashkin on 06.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!!");
+    }
+}
